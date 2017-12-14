3095630770e4bba01def1581dd527d8a8a10271e
Kevin Gilland <kev.gilland93@gmail.com>
2017-12-13 20:51:54 -0500

    sequelized

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
 ### Schema
+DROP DATABASE IF EXISTS quotes_db;
 CREATE DATABASE quotes_db;
 USE quotes_db;
 
@@ -7,7 +8,7 @@ CREATE TABLE quotes
 	id int NOT NULL AUTO_INCREMENT,
 	author varchar(255) NOT NULL,
 	lyrics TEXT NOT NULL,
-	upvote INT(10) NOT NULL
-	downvote INT(10) NOT NULL
+	upvote INT(10) DEFAULT 0,
+	downvote INT(10) DEFAULT 0,
 	PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -11,95 +11,99 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars")
+var hbs = exphbs.create({ defaultLayout: "main" });
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+var db = require('./models')
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "T3ddyT3ssa",
-  database: "quotes_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+db.sequelize.sync().then(() => {
+  console.log('database synchronized')
 });
 
 // Serve index.handlebars to the root route.
 app.get("/", function(req, res) {
-  connection.query("SELECT * FROM quotes;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
+  db.post.findAll().then((posts) => {
+    res.render("index", { posts: posts })
+  })
+  // db.posts.query("SELECT * FROM quotes;", function(err, data) {
+  //   if (err) {
+  //     return res.status(500).end();
+  //   }
 
-    res.render("index", { quotes: data });
-  });
+  //   res.render("index", { quotes: data });
+  // });
 });
+
+app.get('/favicon.ico', function (req, res) {
+  res.status(204).end()
+})
 
 // Show the user the individual quote and the form to update the quote.
 app.get("/:id", function(req, res) {
-  connection.query("SELECT * FROM quotes where id = ?", [req.params.id], function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
+  console.log(req.params)
+  db.post.findOne({ where: { id: req.params.id }})
+  .then((post) => { res.render("single", { post: post.dataValues })})
+  .catch((err) => { res.sendStatus(500)})
+  // connection.query("SELECT * FROM quotes where id = ?", [req.params.id], function(err, data) {
+  //   if (err) {
+  //     return res.status(500).end();
+  //   }
 
-    console.log(data);
-    res.render("single-quote", data[0]);
-  });
+  //   console.log(data);
+  //   res.render("single-quote", data[0]);
+  // });
 });
 
-app.post("/api/quotes", function(req, res) {
-  connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [
-    req.body.author, req.body.quote
-  ], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    }
+app.post("/api/posts", function(req, res) {
+  // connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [
+  //   req.body.author, req.body.quote
+  // ], function(err, result) {
+  //   if (err) {
+  //     // If an error occurred, send a generic server faliure
+  //     return res.status(500).end();
+  //   }
+  db.post.create(req.body).then((post) => { console.log(post); res.json(post); })
 
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+  //   // Send back the ID of the new quote
+  //   res.json({ id: result.insertId });
+  // });
 });
 
 app.delete("/api/quotes/:id", function(req, res) {
-  connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    } else if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+  // connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
+  //   if (err) {
+  //     // If an error occurred, send a generic server faliure
+  //     return res.status(500).end();
+  //   } else if (result.affectedRows == 0) {
+  //     // If no rows were changed, then the ID must not exist, so 404
+  //     return res.status(404).end();
+  //   } else {
+  //     res.status(200).end();
+  //   }
+  // });
+  db.posts.destroy({where: {id: req.params.id}})
+  .then((thing) => { console.log(thing); res.status(200).end() })
 });
 
 
 // Update a quote by an id and then redirect to the root route.
 app.put("/api/quotes/:id", function(req, res) {
-  connection.query("UPDATE quotes SET author = ?, quote = ? WHERE id = ?", [
-    req.body.author, req.body.quote, req.params.id
-  ], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    } else if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+  // connection.query("UPDATE quotes SET author = ?, quote = ? WHERE id = ?", [
+  //   req.body.author, req.body.quote, req.params.id
+  // ], function(err, result) {
+  //   if (err) {
+  //     // If an error occurred, send a generic server faliure
+  //     return res.status(500).end();
+  //   } else if (result.changedRows == 0) {
+  //     // If no rows were changed, then the ID must not exist, so 404
+  //     return res.status(404).end();
+  //   } else {
+  //     res.status(200).end();
+  //   }
+  // });
 });
 
 app.listen(port, function() {
