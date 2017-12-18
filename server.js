4c93f8b8ae88c05483486860590f5688ebe7bb6e
Kevin Gilland <kev.gilland93@gmail.com>
2017-12-18 18:57:57 -0500

    authentication kinda works

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express");
+var session = require("express-session")
 var bodyParser = require("body-parser");
+var flash = require('connect-flash')
 
 var app = express();
 var port = 3000;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+app.use(session({secret: "awesome"}))
+app.use(flash())
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +22,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 var db = require('./models')
+var passport = require('./passport.js')(app)
 
 db.sequelize.sync().then(() => {
   console.log('database synchronized')
@@ -42,7 +47,7 @@ app.get('/favicon.ico', function (req, res) {
 })
 
 // Show the user the individual quote and the form to update the quote.
-app.get("/:id", function(req, res) {
+app.get("api/posts/:id", function(req, res) {
   console.log(req.params)
   db.post.findOne({ where: { id: req.params.id }})
   .then((post) => { res.render("single", { post: post.dataValues })})
@@ -65,12 +70,53 @@ app.post("/api/posts", function(req, res) {
   //     // If an error occurred, send a generic server faliure
   //     return res.status(500).end();
   //   }
-  db.post.create(req.body).then((post) => { console.log(post); res.json(post); })
-
+  if (!req.user) {
+    res.status(401).end()
+  } else {
+    console.log(req.user)
+    var thisPost = {
+      body: req.body.body,
+      author: req.user.dataValues.name
+    }
+    db.post.create(thisPost).then((post) => { console.log(post); res.json(post); })
+  }
   //   // Send back the ID of the new quote
   //   res.json({ id: result.insertId });
   // });
+
 });
+
+app.get('/newUser', function (req, res) {
+  res.render('newUser')
+})
+
+app.post('/newUser', function(req, res) {
+  console.log('here')
+  if (req.body.username && req.body.password) {
+    db.users.findOne({where: {name: req.body.username}}).then(
+      function (user) {
+        console.log(user)
+        if (!user) {
+          db.users.create(
+            { name: req.body.username,
+              password: req.body.password})
+          .then(res.status(200).send('new user created'))
+        } else { res.send('user already exists') }
+      })
+  }
+})
+
+app.get('/login', function (req, res) {
+  res.render('login', {})
+})
+
+app.post('/login',
+  passport.authenticate('local', 
+    { successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true }
+  )
+);
 
 app.delete("/api/quotes/:id", function(req, res) {
   // connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
